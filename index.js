2b044263dc25e9ef07aa99bce7ff5cd496890fd8
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-10-21 18:40:18 -0400

    did the damn thing!

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(arrayOfStrings, eventName){
+    const holdingArray = [];
+    for (let i = 0; i < arrayOfStrings.length; i++){
+        holdingArray.push(`Thank you, ${arrayOfStrings[i]}, for the wonderful ${eventName} gift!`)
+        console.log(i)
+    }
+    return holdingArray
+}
+const names = ['Jim', 'Larry', 'Nandini']
+const Christmas = `Christmas`
+console.log(writeCards(names, Christmas))
+
+
+function countDown(x) {
+    
+    while (x > -1) {
+        console.log(x--)
+    }
+}
+countDown(10)
